bc5b9aa9e28fe6fa2b753410fd18f1c0039d4135
vitalikkk <vitalikgermak6@gmail.com>
2023-06-18 14:38:29 +0300

    add new block

--- pack.js
+++ pack.js
@@ -3,3 +3,24 @@ $(document).ready(function () {
         $(".navigation").toggleClass("active");
     });
 });
+
+
+
+let arrow =  document.querySelector (".arrow")
+let body = document.querySelector("body")
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>10){
+        arrow.style.display="flex"
+    }else{
+        arrow.style.display='none'
+    }
+})
+
+arrow.addEventListener("click",()=>{
+body.scrollIntoView({
+    block:"start",
+    inline:"nearest",
+    behavior:"smooth"
+})
+})
