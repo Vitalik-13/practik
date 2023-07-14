f24d33745bf49ef78e01411800585adc44b36433
vitalikkk <vitalikgermak6@gmail.com>
2023-07-14 13:20:01 +0300

    add new properties

--- pack.js
+++ pack.js
@@ -10,6 +10,87 @@ let arrow =  document.querySelector (".arrow")
 let body = document.querySelector("body")
 const modal = document.querySelector(".reclama")
 const hide = document.querySelector(".absolute")
+const form = document.querySelector(".main-form")
+const inputsForm = document.querySelectorAll(".input")
+let green = document.querySelectorAll(".green-foto");
+
+
+
+
+function rotateElement() {
+    green.forEach((item) => item.classList.add("green-foto-two"));
+  
+  }
+setInterval(rotateElement,2000)
+
+
+
+function removeRotate(){
+    green.forEach((item) =>{
+        if(item.classList.contains('green-foto-two')){
+            item.classList.remove("green-foto-two")
+        }});
+}
+
+setInterval(removeRotate,2100)
+
+
+function sendForm(){
+    let result = true
+    
+
+    function removeTextError(input){
+        let parent = input.parentElement;
+        let errorElement = parent.querySelector('.errorText');
+        if (errorElement) {
+            parent.removeChild(errorElement);
+        }
+        parent.classList.remove('errorText');
+    }
+
+    function removeClassError(item){
+        if(item.classList.contains("error")){
+            item.classList.remove("error")
+        }
+    }
+
+    function addErrorText(input,text){
+        let parent = input.parentElement
+        let errorElement = document.createElement('p')
+        errorElement.textContent=text
+        parent.append(errorElement)
+        errorElement.classList.add('errorText')
+    }
+
+    function addErrorClass(input){
+         input.classList.add("error")
+    }  
+
+
+    inputsForm.forEach((item)=>{
+        removeTextError(item)
+          removeClassError(item)
+        if(item.value==""){
+            addErrorClass(item)
+            addErrorText(item,"Не введені дані")
+            result = false  
+            
+            
+        }
+    })
+
+    return result
+}
+
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+    if(sendForm(this) == true )
+    alert("Форма успішно відправлена")
+    this.reset()
+})
+//========================================
+
+
 
 
 body.addEventListener("click",(event)=>{
@@ -78,15 +159,15 @@ function scrollToSection(e) {
 // Відключення браузерних налаштувань у посилань, що в footer
 
 let manyLink = document.querySelector('.text-on-foto-footer')
-console.log(manyLink)
-
-
 
 manyLink.addEventListener("click",(e)=>{
     if(e.target.closest(".link-foto")){
        e.preventDefault()
     }
 })
+
+
+
   
   
 
